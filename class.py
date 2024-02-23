8e1bfef9d07cbd2476c7f7202e29a9c5e05a5772
Jeremysam23 <153456630+Jeremysam23@users.noreply.github.com>
2024-02-23 20:51:24 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,17 @@
+class num_gen:
+    def __init__(self):
+        self.output = 0
+    def generate(self):
+        self.output += 1
+        return self.output
+    def reset(self):
+        return 0
+    def mul(self, n):
+        self.output = self.output*n
+        return self.output
+
+gen1 = num_gen()
+gen1.generate()
+print(gen1.mul(1))
+print(gen1.mul(6))
+print(gen1.mul(9))
